6de96f9476c3c763b7e0801a010623caeb020ab8
nanthana12 <cattylounphinith@gmail.com>
2023-11-15 13:35:01 +0700

    llllll2

--- src/main/java/com/ldb/truck/Controller/DetailController.java
+++ src/main/java/com/ldb/truck/Controller/DetailController.java
@@ -92,7 +92,7 @@ public class DetailController {
     @CrossOrigin(origins = "*")
     @PostMapping("/saveDataDetails.service")
     public DetailsRes saveDataDetails(@RequestBody  DetailsReq detailsReq){
-        log.info("data:"+detailsReq.getFeeJumPo2());
+        log.info("data999999999:"+detailsReq.getFeeJumPo2());
         DetailsRes result = new DetailsRes();
         try {
             result = detailsService.saveData(detailsReq);
