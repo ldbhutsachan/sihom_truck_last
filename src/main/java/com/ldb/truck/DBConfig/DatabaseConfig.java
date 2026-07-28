da90d2102e52d9c362a8b82495a8f2e89db2be01
veexiong <123249635+veexiong@users.noreply.github.com>
2026-07-28 15:05:33 +0700

    update databaseConfig

--- src/main/java/com/ldb/truck/DBConfig/DatabaseConfig.java
+++ src/main/java/com/ldb/truck/DBConfig/DatabaseConfig.java
@@ -1,30 +1,50 @@
 package com.ldb.truck.DBConfig;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
 import javax.sql.DataSource;
+
 @Configuration
 @EnableJpaRepositories(basePackages = "com.ldb.truck.Repository")
 public class DatabaseConfig {
 
+    @Value("${DB_URL:${spring.datasource.url:jdbc:mariadb://178.128.214.105:33066/khounkham_logistics_db_EABh1xCQuo}}")
+    private String dbUrl;
+
+    @Value("${DB_USERNAME:${spring.datasource.username:khounkham_logistics_user}}")
+    private String dbUsername;
+
+    @Value("${spring.datasource.password:${DB_PASSWORD:}}")
+    private String dbPassword;
+
+    @Value("${spring.datasource.driver-class-name:org.mariadb.jdbc.Driver}")
+    private String dbDriverClassName;
+
     @Bean
     public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
-        dataSource.setUrl("jdbc:mariadb://178.128.214.105:33066/khounkham_logistics_db_EABh1xCQuo");
-        dataSource.setUsername("khounkham_logistics_user");
-        dataSource.setPassword("eBe12345678$DB");
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setDriverClassName(dbDriverClassName);
+        dataSource.setJdbcUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+
+        // HikariCP Connection Pool Settings for High Performance & Production Safety
+        dataSource.setMaximumPoolSize(20);
+        dataSource.setMinimumIdle(5);
+        dataSource.setIdleTimeout(300000); // 5 minutes
+        dataSource.setConnectionTimeout(20000); // 20 seconds
+        dataSource.setMaxLifetime(1200000); // 20 minutes
+
         return dataSource;
     }
+
     @Bean(name = "EBankJdbcTemplate")
-    public JdbcTemplate EBankJdbcTemplate(DataSource EBankJdbcTemplate) {
-        return new JdbcTemplate(EBankJdbcTemplate);
+    public JdbcTemplate EBankJdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 }
